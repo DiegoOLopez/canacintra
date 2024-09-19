9759a725ddbd560ce8014942eff5e6c58b81ce60
Diego_MSI <dieobelopcas01@gmail.com>
2024-09-19 15:01:00 -0600

    Router

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { IndexComponent } from './pages/index/index.component';
+import { NavbarComponent } from './pages/navbar/navbar.component';
 
 export const routes: Routes = [
     {
@@ -8,5 +9,8 @@ export const routes: Routes = [
     },
     {
         path: 'index', component: IndexComponent
+    },
+    {
+        path: 'navbar', component: NavbarComponent
     }
 ];
