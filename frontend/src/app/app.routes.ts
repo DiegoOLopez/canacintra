e617b6cd7867644ac48c250a7ebeee2483929b4f
Diego_MSI <dieobelopcas01@gmail.com>
2024-09-19 14:39:33 -0600

    Implementacion de index en el router

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { IndexComponent } from './pages/index/index.component';
 
 export const routes: Routes = [
     {
         path: 'login', component: LoginComponent
+    },
+    {
+        path: 'index', component: IndexComponent
     }
 ];
