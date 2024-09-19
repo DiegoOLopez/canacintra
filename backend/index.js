397f2731e88b4e34d12f4267144a31db4d0ff62f
Diego_MSI <dieobelopcas01@gmail.com>
2024-09-19 14:18:11 -0600

    Agregado al router el login

---
+++ backend/index.js
@@ -0,0 +1,50 @@
+// Importamos Express
+const express = require('express');
+// Mnadamos a llamar el Router
+const routerAPI = require('./routes');
+const { logErrors, errorHandler, boomErrorHandler, ormErrorHandler } = require('./middlewares/error.handler');
+const {checkApiKey} = require('./middlewares/auth.handler')
+// Instalado?
+const cors = require('cors');
+
+// CONFIGURAR CORS
+const whileList = ['http://localhost:8080', 'https://myapp.co', 'http://localhost:4200'];
+
+// Configurar OPS DE CORS
+const options = {
+  origin: (origin, callback) => {
+    if (whileList.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('no permitido'));
+    }
+  }
+}
+
+
+
+// Inicializamos la API
+const app = express();
+const port = process.env.port || 3000;
+
+app.use(express.json());
+// RUTA
+app.get('/', checkApiKey, (req, res) => {
+  res.send('Hola mi server en express');
+});
+
+// Aplicacion de CORS
+app.use(cors(options));
+
+// ROUTER API
+routerAPI(app);
+
+app.use(logErrors);
+app.use(ormErrorHandler);
+app.use(boomErrorHandler);
+app.use(errorHandler);
+
+// APP LISTEN
+app.listen(port, () => {
+  console.log('Mi port' +  port);
+});
