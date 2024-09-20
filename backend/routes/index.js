f75e7d1ae1090224f7b3e3c157b1c9569955b909
Diego_MSI <dieobelopcas01@gmail.com>
2024-09-20 05:06:06 -0600

    Integracion de index de modelos y prueba de servicios y routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,16 +1,16 @@
 /**
  * AQUI SE AGREGAN TODAS LAS RUTAS DENTRO DE routerApi, exportamos la ruta y la agregamos a la funcion
  */
-
 const express = require('express');
 // Definicion de ruta
+const usuarioRouter = require('./usuario.router');
 
 function routerApi(app) {
   const router = express.Router();
-  app.use('/api/v1', router);
+  app.use('/api', router);
 
   // Agregacion de ruta
-  //router.use('/modulo', moduloRouter);
+  router.use('/usuario', usuarioRouter);
 }
 
 module.exports = routerApi;
