b4e45d505db3a59bba92bf8e3bc2010cd5bed3a4
AlanHerCas <yosky.425@gmail.com>
2024-09-20 06:11:21 -0600

    Correccion del router

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -5,11 +5,11 @@ import { NavbarComponent } from './pages/navbar/navbar.component';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './pages/footer/footer.component';
 import { HomeInversorComponent } from './pages/home-inversor/home-inversor.component';
-<<<<<<< HEAD
+
 import { HomeNegocioComponent } from './pages/home-negocio/home-negocio.component';
-=======
+
 import { RegisterComponent } from './pages/register/register.component';
->>>>>>> f403e5fc0df4c6ac392fb9f9353a0e55ca9ccc00
+
 
 export const routes: Routes = [
     {
@@ -31,10 +31,9 @@ export const routes: Routes = [
         path: 'HomeInversor', component: HomeInversorComponent
     },
     {
-<<<<<<< HEAD
-        path: 'HomeNegocio', component: HomeNegocioComponent
-=======
+       path: 'HomeNegocio', component: HomeNegocioComponent
+    },
+    {
         path: 'register', component: RegisterComponent
->>>>>>> f403e5fc0df4c6ac392fb9f9353a0e55ca9ccc00
     }
 ];
