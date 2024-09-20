9f0c83d8fdadfeae628c757dff191ebbded2ac9f
GonSua20 <cesarin.201433@gmail.com>
2024-09-20 08:41:27 -0600

    Home

---
+++ frontend/src/app/pages/formcontact/formcontact.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formcontact',
+  standalone: true,
+  imports: [],
+  templateUrl: './formcontact.component.html',
+  styleUrl: './formcontact.component.css'
+})
+export class FormcontactComponent {
+
+}
--- frontend/src/app/pages/index/index.component.ts
+++ frontend/src/app/pages/index/index.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-
+import { NavbarComponent } from '../navbar/navbar.component';
+import { FooterComponent } from '../footer/footer.component';
 @Component({
   selector: 'app-index',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent, FooterComponent],
   templateUrl: './index.component.html',
   styleUrl: './index.component.css'
 })
