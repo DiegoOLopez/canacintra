956be4a4cc9b91757000d3ae31d0237159901ed0
AlanHerCas <yosky.425@gmail.com>
2024-09-20 11:22:19 -0600

    Home negocio diseño terminado

--- frontend/src/app/pages/home-negocio/home-negocio.component.ts
+++ frontend/src/app/pages/home-negocio/home-negocio.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-home-negocio',
   standalone: true,
-  imports: [ NavbarComponent ],
+  imports: [NavbarComponent, FooterComponent],
   templateUrl: './home-negocio.component.html',
   styleUrl: './home-negocio.component.css'
 })
