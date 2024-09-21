93428ab58e5706b4051bcd629cc6b696c187c08a
GonSua20 <cesarin.201433@gmail.com>
2024-09-21 11:54:03 -0600

    servicio

--- frontend/src/app/pages/formulario/formulario.component.ts
+++ frontend/src/app/pages/formulario/formulario.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../footer/footer.component';
 import { NavbarsesionComponent } from "../navbarsesion/navbarsesion.component";
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-formulario',
   standalone: true,
-  imports: [FooterComponent, NavbarsesionComponent],
+  imports: [CommonModule,FooterComponent, NavbarsesionComponent],
   templateUrl: './formulario.component.html',
   styleUrl: './formulario.component.css'
 })
