860993388d14dc62188757d0d321cec569bbfe0a
Jean Bonilla <snapjean790@gmail.com>
2024-09-21 12:14:16 -0600

    cosasNuevas

--- frontend/src/app/servicios/solicitud/solicitud.service.ts
+++ frontend/src/app/servicios/solicitud/solicitud.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class SolicitudService {
+export class SolicitudService{
 
-  constructor() { }
+  private apiUrl = 'http://localhost:3000/api/propuesta';  // Asegúrate de que esta URL sea correcta y accesible.
+
+  constructor(private http: HttpClient) {}
+
+  // Método para obtener propuestas desde la API
+  getSolicitud(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
+    }
 }
